cf4dace7f53aa68744550f27f4bcd2e1fc171b17
denis5417 <denisz.pol@yandex.ru>
2017-07-20 12:40:34 +0300

    Написан скрипт и README

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Скрипт расчитывает
+1. Самый большой бар
+2. Самый маленький бар
+3. Самый близкий бар
+
+Работает с данными [отсюда](https://apidata.mos.ru/v1/datasets/1796/rows?api_key=e1a2169e5e05bdbcf1b768af34764fd7)
 
 # Как запустить
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 и библиотеку [geopy](https://github.com/geopy/geopy)
+
+Установка библиотеки:
+
+```#!bash
+pip install -r requirements.txt
+```
 
 Запуск на Linux:
 
 ```#!bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python3 bars.py <path_to_file_with_JSON_data> <longitude> <latitude>
+Самый большой бар – Спорт бар «Красная машина»
+Самый маленький бар - БАР. СОКИ
+Самый близкий бар – Таверна
+
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,39 @@
 import json
+import sys
+from geopy.distance import vincenty
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath) as file_handler:
+            return json.load(file_handler)
+    except json.decoder.JSONDecodeError:
+        print("В файле не JSON")
+    except FileNotFoundError:
+        print("Файл не найден")
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bar_data):
+    return max(bar_data, key=lambda bar: bar['Cells']['SeatsCount'])['Cells']['Name']
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bar_data):
+    return min(bar_data, key=lambda bar: bar['Cells']['SeatsCount'])['Cells']['Name']
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bar_data, longitude, latitude):
+    find_distance = lambda bar: vincenty(bar["Cells"]['geoData']['coordinates'], [longitude, latitude]).km
+    return min(bar_data, key=find_distance)['Cells']['Name']
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 3:
+        filepath = sys.argv[1]
+        longitude = sys.argv[2]
+        latitude = sys.argv[3]
+        bar_data = load_data(filepath)
+        print("Самый большой бар – {}".format(get_biggest_bar(bar_data)))
+        print("Самый маленький бар - {}".format(get_smallest_bar(bar_data)))
+        print("Самый близкий бар – {}".format(get_closest_bar(bar_data, longitude, latitude)))
+    else:
+        print("Введите путь до файла, и координаты")
